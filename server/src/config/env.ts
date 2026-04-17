da77660455e95bbb00176d222b98392692dce699
edisonmalasan <malasanedison1@gmail.com>
2026-04-17 14:22:59 +0800

    feat(backend): add GOOGLE & Github env prop

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -1,7 +1,7 @@
 import { z } from "zod";
 import dotenv from "dotenv";
 
-// load environment variables from .env file
+// load env
 dotenv.config();
 
 const envSchema = z.object({
@@ -12,9 +12,16 @@ const envSchema = z.object({
   CLIENT_URL: z.string().default("http://localhost:5173"),
 
   // database
-  DATABASE_URL: z.string().optional(), // optional for now
+  DATABASE_URL: z.string().optional(), // optionakl because prisma can also read from .env directly
 
-  // later
+  // oauth
+  GOOGLE_CLIENT_ID: z.string(),
+  GOOGLE_CLIENT_SECRET: z.string(),
+  GITHUB_CLIENT_ID: z.string(),
+  GITHUB_CLIENT_SECRET: z.string(),
+  GITHUB_CALLBACK_URL: z.string(),
+
+  // later can add more env vars likke JWT secrets, Redis URL, etc.
   // JWT_ACCESS_SECRET: z.string(),
   // REDIS_URL: z.string(),
 });
