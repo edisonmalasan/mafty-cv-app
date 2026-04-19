02da673c85480b5913c15821223478a9f3d29cf0
edisonmalasan <malasanedison1@gmail.com>
2026-04-19 21:04:45 +0800

    feat(server): add user schema

---
+++ server/src/modules/user/user.schema.ts
@@ -0,0 +1,10 @@
+import z from "zod";
+
+export const updateUserSchema = z.object({
+  body: z.object({
+    name: z.string().optional(),
+    email: z.string().email().optional(),
+  }),
+});
+
+export type UpdateUserInput = z.infer<typeof updateUserSchema>["body"];
